d71c0c6255d169cb13fa66af319fb885c97ae12f
smltng <sat83@cornell.edu>
2019-03-01 21:54:41 -0500

    Updates

---
+++ precache-manifest.34f79b17f76404f4ca2b45ecb1ebf5a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a79160c98ee2f4651670",
+    "url": "/github-page/static/js/runtime~main.a79160c9.js"
+  },
+  {
+    "revision": "b592304e36df496bbc38",
+    "url": "/github-page/static/js/main.b592304e.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/github-page/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "b592304e36df496bbc38",
+    "url": "/github-page/static/css/main.3e2c7c6e.chunk.css"
+  },
+  {
+    "revision": "b3908a733d67e1cb680b98a418a4f370",
+    "url": "/github-page/index.html"
+  }
+];
